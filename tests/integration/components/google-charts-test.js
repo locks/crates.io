e0112e812e6d05defed630cfb80758178cba14f2
Ricardo Mendes <rokusu@gmail.com>
2017-06-17 21:00:21 +0100

    move google charts init to a component

---
+++ tests/integration/components/google-charts-test.js
@@ -0,0 +1,25 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('google-charts', 'Integration | Component | google charts', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{google-charts}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#google-charts}}
+      template block text
+    {{/google-charts}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
